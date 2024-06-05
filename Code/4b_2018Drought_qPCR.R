a0df470b4701fdf59e33ee7b48e0a0e0a24ce14a
Anna Burns <anna.burns@stud.uni-greifswald.de>
2024-06-05 09:37:54 +0000

    qpcr figs

--- Code/4b_2018Drought_qPCR.R
+++ Code/4b_2018Drought_qPCR.R
@@ -49,18 +49,20 @@ ggplot(qpcr.summary[qpcr.summary$Site == 'PW',], aes(x = Date, y = mean, col = D
   theme_classic() + 
   geom_errorbar(aes(ymax = mean + se, ymin = mean, color = Domain), width = 0.1, linewidth = 1) +
   scale_shape_manual(values = c(15,17)) + 
-  scale_color_manual(values = c("#A04000","#117A65","#7D3C98")) + 
+  scale_color_manual(values = c("#A04000","#117A65")) + 
   scale_y_continuous(limits = c(0, 3e8), breaks = c(0, 1e8, 2e8, 3e8), labels = c('0.0', '1.0', '2.0', '3.0')) +
   scale_x_discrete(drop = FALSE) + 
   labs(y = expression(paste(x10^{8}, ' copies/g DW soil')), x = '') + 
   theme(axis.title=element_text(size=20,face = "bold"),
         axis.title.y=element_text(margin = margin(l = 60)),
+        axis.text.x = element_text(angle=45, vjust = 0.6),
         axis.text=element_text(size=20,face = "bold"),
         title = element_text(size = 20, face = 'bold'),
         panel.border = element_rect(linetype = "solid", colour = "black", fill = NA, size=2),
         panel.background = element_rect(fill = NA),panel.grid.major = element_blank(),legend.position = c(0.9,0.9),
         legend.text = element_text(size = 15),legend.key = element_rect(fill = NA),legend.title = element_blank(),
-        legend.background = element_blank())
+        legend.background = element_blank(),
+        aspect.ratio = 1)
 
 kruskal.test(Abundance~Date, data = qpcr[qpcr$Domain == 'AOB' & qpcr$Site == 'PW',])
 kruskal_effsize(Abundance~Date, data = qpcr[qpcr$Domain == 'AOB' & qpcr$Site == 'PW',])
@@ -74,18 +76,20 @@ ggplot(qpcr.summary[qpcr.summary$Site == 'PD',], aes(x = Date, y = mean, col = D
   theme_classic() + 
   geom_errorbar(aes(ymax = mean + se, ymin = mean, color = Domain), width = 0.1, linewidth = 1) +
   scale_shape_manual(values = c(15,17)) + 
-  scale_color_manual(values = c("#A04000","#117A65","#7D3C98")) + 
+  scale_color_manual(values = c("#A04000","#117A65")) + 
   scale_y_continuous(limits = c(0, 6.6e8), breaks = c(0, 2e8, 4e8, 6e8), labels = c('0.0', '2.0', '4.0', '6.0')) +
   scale_x_discrete(drop = FALSE) + 
   labs(y = expression(paste(x10^{8}, ' copies/g DW soil')), x = '') + 
   theme(axis.title=element_text(size=20,face = "bold"),
         axis.title.y=element_text(margin = margin(l = 60)),
+        axis.text.x = element_text(angle=45, vjust = 0.6),
         axis.text=element_text(size=20,face = "bold"),
         title = element_text(size = 20, face = 'bold'),
         panel.border = element_rect(linetype = "solid", colour = "black", fill = NA, size=2),
         panel.background = element_rect(fill = NA),panel.grid.major = element_blank(),legend.position = c(0.9,0.9),
         legend.text = element_text(size = 15),legend.key = element_rect(fill = NA),legend.title = element_blank(),
-        legend.background = element_blank())
+        legend.background = element_blank(),
+        aspect.ratio = 1)
 
 kruskal.test(Abundance~Date, data = qpcr[qpcr$Domain == 'AOB' & qpcr$Site == 'PD',])
 kruskal_effsize(Abundance~Date, data = qpcr[qpcr$Domain == 'AOB' & qpcr$Site == 'PD',])
@@ -99,18 +103,20 @@ ggplot(qpcr.summary[qpcr.summary$Site == 'CW',], aes(x = Date, y = mean, col = D
   theme_classic() + 
   geom_errorbar(aes(ymax = mean + se, ymin = mean, color = Domain), width = 0.1, linewidth = 1) +
   scale_shape_manual(values = c(15,17)) + 
-  scale_color_manual(values = c("#A04000","#117A65","#7D3C98")) + 
+  scale_color_manual(values = c("#A04000","#117A65")) + 
   scale_y_continuous(limits = c(0, 1.2e8), breaks = c(0, 0.5e8, 1e8), labels = c('0.0', '0.5', '1.0')) +
   scale_x_discrete(drop = FALSE) + 
   labs(y = expression(paste(x10^{8}, ' copies/g DW soil')), x = '') + 
   theme(axis.title=element_text(size=20,face = "bold"),
         axis.title.y=element_text(margin = margin(l = 60)),
+        axis.text.x = element_text(angle=45, vjust = 0.6),
         axis.text=element_text(size=20,face = "bold"),
         title = element_text(size = 20, face = 'bold'),
         panel.border = element_rect(linetype = "solid", colour = "black", fill = NA, size=2),
         panel.background = element_rect(fill = NA),panel.grid.major = element_blank(),legend.position = c(0.9,0.9),
         legend.text = element_text(size = 15),legend.key = element_rect(fill = NA),legend.title = element_blank(),
-        legend.background = element_blank())
+        legend.background = element_blank(),
+        aspect.ratio = 1)
 
 kruskal.test(Abundance~Date2, data = qpcr[qpcr$Domain == 'AOA' & qpcr$Site == 'CW',])
 kruskal_effsize(Abundance~Date, data = qpcr[qpcr$Domain == 'AOB' & qpcr$Site == 'CW',])
--- Code/4d_2018Drought_Metatranscriptomics.R
+++ Code/4d_2018Drought_Metatranscriptomics.R
@@ -40,11 +40,17 @@ aoa$month <- factor(aoa$month, levels = c('18-Apr', '18-Jun', '18-Aug', '18-Oct'
 
 aoa_summary <- aoa %>% group_by(Site, month, Subunit) %>% summarise(mean = mean(Amount), sd = sd(Amount), n = n(), se = sd/sqrt(n))
 
+aoa_summary$month <- factor(aoa_summary$month, levels = c('18-Apr', '18-Jun', '18-Aug', '18-Oct', '18-Dec', '19-Feb'), 
+                              labels = c('2018-04', '2018-06', '2018-08', '2018-10', '2018-12', '2019-02'))
+
 # preparing aob data
 
 aob$month <- factor(aob$month, levels = c('18-Apr', '18-Jun', '18-Aug', '18-Oct', '18-Dec', '19-Feb'))
 
 aob_summary <- aob %>% group_by(Site, month, Subunit) %>% summarise(mean = mean(Amount), sd = sd(Amount), n = n(), se = sd/sqrt(n))
+
+aob_summary$month <- factor(aob_summary$month, levels = c('18-Apr', '18-Jun', '18-Aug', '18-Oct', '18-Dec', '19-Feb'), 
+                            labels = c('2018-04', '2018-06', '2018-08', '2018-10', '2018-12', '2019-02'))
 
 ################################################################################
 # Figures and Statistics                                                       #
@@ -61,17 +67,19 @@ ggplot(aoa_summary[aoa_summary$Site == 'CW',], aes(x = month, y = mean, col = Su
   geom_errorbar(aes(ymax = mean + se, ymin = mean, color = Subunit), width = 0.1, linewidth = 1) +
   scale_shape_manual(values = c(15,16,17)) + 
   scale_color_manual(values = c("#A04000","#117A65","#7D3C98")) + 
-  scale_y_continuous(limits = c(0, 1e7), breaks = c(0, 3e6, 6e6, 9e6), labels = c(0.0,3.0, 6.0,9.0)) +
+  scale_y_continuous(limits = c(0, 1e7), breaks = c(0, 3e6, 6e6, 9e6), labels = c('0.0','3.0', '6.0','9.0')) +
   scale_x_discrete(drop = FALSE) + 
   labs(y = expression(paste(x10^{6}, ' copies/g DW soil')), x = '') + 
   theme(axis.title=element_text(size=20,face = "bold"),
         axis.title.y=element_text(margin = margin(l = 60)),
+        axis.text.x = element_text(angle=45, vjust = 0.6),
         axis.text=element_text(size=20,face = "bold"),
         title = element_text(size = 20, face = 'bold'),
         panel.border = element_rect(linetype = "solid", colour = "black", fill = NA, size=2),
-        panel.background = element_rect(fill = NA),panel.grid.major = element_blank(),legend.position = c(0.9,0.9),
+        panel.background = element_rect(fill = NA),panel.grid.major = element_blank(),legend.position = c(0.85,0.85),
         legend.text = element_text(size = 15),legend.key = element_rect(fill = NA),legend.title = element_blank(),
-        legend.background = element_blank())
+        legend.background = element_blank(),
+        aspect.ratio = 1)
 
 ## percolation wet site
 
@@ -82,16 +90,18 @@ ggplot(aoa_summary[aoa_summary$Site == 'PW',], aes(x = month, y = mean, col = Su
   geom_errorbar(aes(ymax = mean + se, ymin = mean, color = Subunit), width = 0.1, linewidth = 1) +
   scale_shape_manual(values = c(15,16,17)) + 
   scale_color_manual(values = c("#A04000","#117A65","#7D3C98")) + 
-  scale_y_continuous(limits = c(0, 1.7e8), breaks = c(0, 5e7, 1e8, 1.5e8), labels = c(0,0.5,1.0,1.5)) +
+  scale_y_continuous(limits = c(0, 1.7e8), breaks = c(0, 5e7, 1e8, 1.5e8), labels = c('0.0','0.5','1.0','1.5')) +
   scale_x_discrete(drop = FALSE) + 
   labs(x = '', y = expression(paste(x10^{8}, ' copies/g DW soil'))) +
   theme(axis.title=element_text(size=20,face = "bold"),axis.text=element_text(size=20,face = "bold"),
         title = element_text(size = 20, face = 'bold'),
+        axis.text.x = element_text(angle=45, vjust = 0.6),
         axis.title.y=element_text(margin = margin(l = 60)),
         panel.border = element_rect(linetype = "solid", colour = "black", fill = NA, size=2),
-        panel.background = element_rect(fill = NA),panel.grid.major = element_blank(),legend.position = c(0.9,0.9),
+        panel.background = element_rect(fill = NA),panel.grid.major = element_blank(),legend.position = c(0.85,0.85),
         legend.text = element_text(size = 15),legend.key = element_rect(fill = NA),legend.title = element_blank(),
-        legend.background = element_blank())
+        legend.background = element_blank(),
+        aspect.ratio = 1)
 
 # aob
 
@@ -104,16 +114,18 @@ ggplot(aob_summary[aob_summary$Site == 'CW',], aes(x = month, y = mean, col = Su
   geom_errorbar(aes(ymax = mean + se, ymin = mean, color = Subunit), width = 0.1, linewidth = 1) +
   scale_shape_manual(values = c(15,16,17)) + 
   scale_color_manual(values = c("#117A65","#7D3C98")) + 
-  scale_y_continuous(limits = c(0, 3e6), breaks = c(0, 1e6, 2e6, 3e6), labels = c(0,1,2,3)) +
+  scale_y_continuous(limits = c(0, 2.2e6), breaks = c(0, 0.5e6, 1e6, 1.5e6,2e6), labels = c('0.0','0.5','1.0','1.5', '2.0')) +
   scale_x_discrete(drop = FALSE) + 
   labs(x = '', y = expression(paste(x10^{6}, ' copies/g DW soil'))) +
   theme(axis.title=element_text(size=20,face = "bold"),axis.text=element_text(size=20,face = "bold"),
         title = element_text(size = 20, face = 'bold'),
+        axis.text.x = element_text(angle=45, vjust = 0.6),
         axis.title.y=element_text(margin = margin(l = 60)),
         panel.border = element_rect(linetype = "solid", colour = "black", fill = NA, size=2),
-        panel.background = element_rect(fill = NA),panel.grid.major = element_blank(),legend.position = c(0.9,0.9),
+        panel.background = element_rect(fill = NA),panel.grid.major = element_blank(),legend.position = c(0.85,0.9),
         legend.text = element_text(size = 15),legend.key = element_rect(fill = NA),legend.title = element_blank(),
-        legend.background = element_blank())
+        legend.background = element_blank(),
+        aspect.ratio = 1)
 
 # percolation wet site
 
@@ -124,13 +136,15 @@ ggplot(aob_summary[aob_summary$Site == 'PW',], aes(x = month, y = mean, col = Su
   geom_errorbar(aes(ymax = mean + se, ymin = mean, color = Subunit), width = 0.1, linewidth = 1) +
   scale_shape_manual(values = c(15,16,17)) + 
   scale_color_manual(values = c("#117A65","#7D3C98")) + 
-  scale_y_continuous(limits = c(0,7e6), breaks = c(0, 3e6, 6e6), labels = c(0,3,6)) +
+  scale_y_continuous(limits = c(0,7e6), breaks = c(0, 3e6, 6e6), labels = c('0.0','3.0','6.0')) +
   scale_x_discrete(drop = FALSE) + 
   labs(x = '', y = expression(paste(x10^{6}, ' copies/g DW soil'))) +
   theme(axis.title=element_text(size=20,face = "bold"),axis.text=element_text(size=20,face = "bold"),
         title = element_text(size = 20, face = 'bold'),
+        axis.text.x = element_text(angle=45, vjust = 0.6),
         axis.title.y=element_text(margin = margin(l = 60)),
         panel.border = element_rect(linetype = "solid", colour = "black", fill = NA, size=2),
-        panel.background = element_rect(fill = NA),panel.grid.major = element_blank(),legend.position = c(0.9,0.9),
+        panel.background = element_rect(fill = NA),panel.grid.major = element_blank(),legend.position = c(0.85,0.91),
         legend.text = element_text(size = 15),legend.key = element_rect(fill = NA),legend.title = element_blank(),
-        legend.background = element_blank())
+        legend.background = element_blank(),
+        aspect.ratio = 1)
